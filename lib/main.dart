ec1d511adcba31adff9d0a48ff29887a14b5dc16
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-10 00:09:35 +0900

    クラスとインスタンス化

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class PixabayPage extends StatefulWidget {
 
 class _PixabayPageState extends State<PixabayPage> {
   // 初期値は空のListを与えます。
-  List imageList = [];
+  List<PixabayImage> pixabayImages = [];
 
   // 非同期の関数になったため返り値の型にFutureがつき、さらに async キーワードが追加されました。
   Future<void> fetchImages(String text) async {
@@ -42,9 +42,31 @@ class _PixabayPageState extends State<PixabayPage> {
         'per_page': 100,
       },
     );
-    // 用意した imageList に hits の value を代入する
-    imageList = response.data['hits'];
+    // この時点では要素の中身の型は Map<String, dynamic>
+    final List hits = response.data['hits'];
+    // map メソッドを使って Map<String, dynamic> の型を一つひとつ PixabayImage 型に変換していきます。
+    pixabayImages = hits.map((e) => PixabayImage.fromMap(e)).toList();
     setState(() {}); // 画面を更新したいので setState も呼んでおきます
+  }
+
+  Future<void> shareImage(String url) async {
+    // まずは一時保存に使えるフォルダ情報を取得します。
+    // Future 型なので await で待ちます
+    final dir = await getTemporaryDirectory();
+
+    final response = await Dio().get(
+      url,
+      options: Options(
+        // 画像をダウンロードするときは ResponseType.bytes を指定します。
+        responseType: ResponseType.bytes,
+      ),
+    );
+    // フォルダの中に image.png という名前でファイルを作り、そこに画像データを書き込みます。
+    final imageFile =
+        await File('${dir.path}/image.png').writeAsBytes(response.data);
+
+    // path を指定すると share できます。
+    await Share.shareFiles([imageFile.path]);
   }
 
   // この関数の中の処理は初回に一度だけ実行されます。
@@ -77,43 +99,26 @@ class _PixabayPageState extends State<PixabayPage> {
         ),
         // itemCount には要素数を与えます。
         // List の要素数は .length で取得できます。今回は20になります。
-        itemCount: imageList.length,
+        itemCount: pixabayImages.length,
         // index には 0 ~ itemCount - 1 の数が順番に入ってきます。
         // 今回、要素数は 20 なので 0 ~ 19 が順番に入ります。
         itemBuilder: (context, index) {
           // 要素を順番に取り出します。
           // index には 0 ~ 19 の値が順番に入ること、
           // List から番号を指定して要素を取り出す書き方を思い出しながら眺めてください。
-          Map<String, dynamic> image = imageList[index];
+          final pixabayImage = pixabayImages[index];
           // プレビュー用の画像データがあるURLは previewURL の value に入っています。
           // URLをつかった画像表示は Image.network(表示したいURL) で実装できます。
           return InkWell(
             onTap: () async {
-              // まずは一時保存に使えるフォルダ情報を取得します。
-              // Future 型なので await で待ちます
-              final dir = await getTemporaryDirectory();
-
-              final response = await Dio().get(
-                // previewURL は荒いためより高解像度の webformatURL から画像をダウンロードします。
-                image['webformatURL'],
-                options: Options(
-                  // 画像をダウンロードするときは ResponseType.bytes を指定します。
-                  responseType: ResponseType.bytes,
-                ),
-              );
-              // フォルダの中に image.png という名前でファイルを作り、そこに画像データを書き込みます。
-              final imageFile = await File('${dir.path}/image.png')
-                  .writeAsBytes(response.data);
-
-              // path を指定すると share できます。
-              await Share.shareFiles([imageFile.path]);
+              shareImage(pixabayImage.webformatURL);
             },
             child: Stack(
               // StackFit.expand を与えると領域いっぱいに広がろうとします。
               fit: StackFit.expand,
               children: [
                 Image.network(
-                  image['previewURL'],
+                  pixabayImage.previewURL,
                   // BoxFit.cover を与えると領域いっぱいに広がろうとします。
                   fit: BoxFit.cover,
                 ),
@@ -131,7 +136,7 @@ class _PixabayPageState extends State<PixabayPage> {
                           Icons.thumb_up_alt_outlined,
                           size: 14,
                         ),
-                        Text('${image['likes']}'),
+                        Text('${pixabayImage.likes}'),
                       ],
                     ),
                   ),
@@ -141,6 +146,26 @@ class _PixabayPageState extends State<PixabayPage> {
           );
         },
       ),
+    );
+  }
+}
+
+class PixabayImage {
+  final String previewURL;
+  final int likes;
+  final String webformatURL;
+
+  PixabayImage({
+    required this.previewURL,
+    required this.likes,
+    required this.webformatURL,
+  });
+
+  factory PixabayImage.fromMap(Map<String, dynamic> map) {
+    return PixabayImage(
+      previewURL: map['previewURL'],
+      likes: map['likes'],
+      webformatURL: map['webformatURL'],
     );
   }
 }
